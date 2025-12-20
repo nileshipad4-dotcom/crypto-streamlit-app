2997a44ffecb250be787ee050b1f800bd2df0509
nilesh raj <nileshipad4@gmail.com>
2025-12-20 13:33:26 +0530

    Update collector.py

--- collector.py
+++ collector.py
@@ -6,7 +6,7 @@ import os
 
 headers = {"Accept": "application/json"}
 
-EXPIRIES = ["20-12-2025"]
+EXPIRIES = ["21-12-2025"]
 UNDERLYINGS = ["BTC", "ETH"]
 
 API_DELTA = "https://api.india.delta.exchange/v2/tickers"
